f6a150dbadb0127f84eb6c072f4a213ff01f7a16
unrealJuanpa <juanpacloud@gmail.com>
2022-10-08 13:30:31 -0600

    Add project files.

---
+++ WinUI/FormLogin.cs
@@ -0,0 +1,15 @@
+namespace WinUI
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
